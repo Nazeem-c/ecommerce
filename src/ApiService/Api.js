8d360bcde97a5bbcc699c172500c855e0560646b
NazeemC <mhdnazeemc@gmail.com>
2023-08-04 15:39:30 +0530

    Api integration

--- src/ApiService/Api.js
+++ src/ApiService/Api.js
@@ -1,3 +1,18 @@
 import axios from 'axios';
 
 const BASE_URL = "https://fakestoreapi.com/"
+
+export const getAllProducts =async()=>{
+const response = await axios.get('`${BASE_URL}/products')
+return response.data;
+}
+
+export const getProductId =async()=>{
+    const response = await axios.get('`${BASE_URL}/products/${id}')
+    return response.data;
+    }
+
+export const getProductImage =async()=>{
+    const response = await axios.get('`${BASE_URL}/products/${id}/image')
+    return response.data;
+    }    
