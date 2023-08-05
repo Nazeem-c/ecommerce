2beb06e3387a5e621845e513f11608703533a5ca
NazeemC <mhdnazeemc@gmail.com>
2023-08-05 12:00:42 +0530

    FInal Commit

--- src/ApiService/Api.js
+++ src/ApiService/Api.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
-const BASE_URL = "https://fakestoreapi.com/"
+// const BASE_URL = "https://fakestoreapi.com/"
 
 export const getAllProducts =async()=>{
-const response = await axios.get('`${BASE_URL}/products')
+const response = await axios.get('https://fakestoreapi.com/products')
 return response.data;
 }
 
 export const getProductId =async()=>{
-    const response = await axios.get('`${BASE_URL}/products/${id}')
+    const response = await axios.get('https://fakestoreapi.com/products/${id}')
     return response.data;
     }
 
 export const getProductImage =async()=>{
-    const response = await axios.get('`${BASE_URL}/products/${id}/image')
+    const response = await axios.get('https://fakestoreapi.com/products/${id}/image')
     return response.data;
-    }    
+        }    
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import './Home.css'
+import { getAllProducts } from '../../ApiService/Api'
 
 function Home() {
+    const [product, setProduct] = useState([]);
+    useEffect(() => {
+      const fetchProduct = async()=>{
+        const data = await getAllProducts();
+        setProduct(data)
+      }
+     fetchProduct() 
+    }, []);
+    
   return (
-    <div>Home</div>
+    <div className='home'>
+        {
+                product.map((obj,index)=>{
+                    return(
+                        <div className='product' key={obj.id}>
+            
+                    <img src={obj.image} alt="alter" />
+                    <h2>{obj.title}</h2>
+                    <p>
+                    <span className='price'>${obj.price}</span>
+                    </p>
+                    <button>Product Details</button>
+                    
+                </div>
+                    )
+                })
+            }
+       
+
+    </div>
   )
 }
 
